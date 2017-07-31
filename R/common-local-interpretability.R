67c7eefa58f89037fc46e41b8c942319aa2842a1
yvanhuele <yannick.vanhuele@healthcatalyst.com>
2017-07-31 08:13:11 -0600

    Added function to get local dataframe

---
+++ R/common-local-interpretability.R
@@ -0,0 +1,53 @@
+#' @title
+#' Get a dataframe consisting of rows "near" a given row
+#'
+#' @description Build a dataframe consisting of rows which are perturbations 
+#' of a given row.
+#' @param baseRow a row of data (in a dataframe) to perturb
+#' @param modifiableCols a vector of column names corresponding to the columns
+#' which should be perturbed
+#' @param info a list which indexed by the column names in modifiableCols 
+#' containing the standard deviation (for numeric columns) or the factor levels
+#' (for factor columns)
+#' @param size the number of rows in
+#' @param spread
+#' @param grainCol
+#' @param predictedCol
+#' 
+#' @return a dataframe consisting whose rows are perturbations of baseRow
+#' 
+#'
+#' @export
+#' @references \url{http://healthcare.ai}
+#' @seealso \code{\link{healthcareai}}
+localPerturbations = function(baseRow, 
+                              modifiableCols,
+                              info,
+                              size = 1000,
+                              spread = 1/4,
+                              grainCol = NA, 
+                              predictedCol = NA) {
+  # dummy column to set number of rows for dataframe
+  df <- data.frame(temp_column_to_drop = rep(0, times = size))
+  for (col in names(baseRow)) {
+    if (is.na(grainCol) || col != grainCol) {
+      mu <- baseRow[[col]]
+      # if modifiable, fill with random values near mu
+      if (col %in% modifiableCols) {
+        # if numeric, sample using a normal distribution
+        if (is.numeric(mu)) {
+          sd <- info[[col]]
+          df[[col]] <- rnorm(size, mean = mu, sd = spread*sd)
+        } else {# not numeric
+          levels <- factor(info[[col]], ordered = F)
+          df[[col]] <- sample(levels, replace = T, size = size)
+        }
+      } else {# not modifiable -> repeat mu throughout
+        df[[col]] <- rep(mu, times = size)
+      }
+    }
+  }
+  # drop temp column
+  df$temp_column_to_drop <- NULL
+  return(df)
+}
