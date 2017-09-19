3bc38e48f8f18ded1569f1b91d54a2c288bcaefb
Taylor Miller <taylor.miller@healthcatalyst.com>
2017-09-19 14:55:01 -0600

    * cleanup for 2017 * changed a bunch of variable names, lots of linting, lots of commenting and calling out TODOS for 2018

--- common-profile-cleaner.R
+++ common-profile-cleaner.R
@@ -1,56 +1,64 @@
-ageCleaner <- function(ages) {
-	cleanAges <- c()
-
-	for (age in ages){
-		# extract out integers from garbage data, may see NAs here
-		trimmedAge <- as.numeric(regmatches(age, regexpr("[0-9]*", age)))
-
-		# Note append is not inplace
-		cleanAges <- append(cleanAges, trimmedAge)
-	}
-
-	# fill in all nas with the mean
-	cleanAges[is.na(cleanAges)] <- mean(cleanAges, na.rm=TRUE)
-
-	return(cleanAges)
+# This function takes in the free text field that asked for an integer age which of course
+# people put all sorts of stuff into. This should be sanitized on the mobile end, but
+# we can easily deal with dirty data here.
+sanitize_integers_in_free_text <- function(ages) {
+    cleanAges <- c()
+    
+    for (age in ages) {
+        # extract out integers from garbage data, may see NAs here
+        trimmed_age <- as.numeric(regmatches(age, regexpr("[0-9]*", age)))
+        
+        # Note append is not inplace
+        cleanAges <- append(cleanAges, trimmed_age)
+    }
+    
+    # fill in all nas with the mean
+    cleanAges[is.na(cleanAges)] <- mean(cleanAges, na.rm = TRUE)
+    
+    return(cleanAges)
 }
 
-dashCleaner <- function(df) {
-	# cleaning out dashes which are used in the variance hack
-	for (num in 1:ncol(df)) { 
-		options <- levels(df[,num])
-		newLevels <- c()
-		for (o in options) {
-			o <- gsub('-', '_', o)
-			newLevels <- append(newLevels, o)
-		}
-		levels(df[,num]) <- newLevels
-	}
-	return(df)
+
+remove_dashes_from_strings <- function(df) {
+    # cleaning out dashes which are used in the findingVariation in r 1.0 release
+    for (num in 1:ncol(df)) {
+        options <- levels(df[, num])
+        newLevels <- c()
+        for (o in options) {
+            o <- gsub("-", "_", o)
+            newLevels <- append(newLevels, o)
+        }
+        levels(df[, num]) <- newLevels
+    }
+    return(df)
 }
 
-cleanProfiles <- function(df){
 
+clean_profiles <- function(df){
+	# Clean all 2017 profile data
+	#
+	# note that there is a lot of hard coded values that set the ordinal order. There
+	# likely isn't a way around this.
+	# TODO next year talk to mobile team about ordering in the app - there MUST be an order value
+
+	# Nuke a few columns
 	columnsToRemove <- c(
 		"UserID",
 		"UserFullNM",
 		"QuestionDSC",
 		"AnswerDSC"
 		)
-
 	for (column in columnsToRemove){
 		if (!(is.null(df[[column]]))) {
 			df[[column]] <- as.character(df[[column]])
 		}
 	}
-
-	# delete problem columns
 	df$QuestionID <- NULL
 	df$QuestionDSC <- NULL
 	df$AnswerDSC <- NULL
 	df$AnswerOrderNBR <- NULL
 
-	# healthcare experience
+	# 2017 Profile question healthcare: experience
 	df[,4] <- factor(df[,4], levels=c(
 		"Not applicable",
 		"Over 30 years", 
@@ -61,7 +69,7 @@ cleanProfiles <- function(df){
 		),
 	ordered=TRUE)
 
-	# Position
+	# 2017 Profile question Position:
 	df[,5] <- factor(df[,5], levels=c(
 		"Other or not applicable",
 		"Front line (e.g., analyst, engineer)",
@@ -71,7 +79,7 @@ cleanProfiles <- function(df){
 		),
 		ordered=TRUE)
 
-	# EDW Success
+	# 2017 Profile question EDW: Success
 	df[,8] <- factor(df[,8], levels=c(
 		"Unsure or not applicable",
 		"No",
@@ -80,7 +88,7 @@ cleanProfiles <- function(df){
 		),
 		ordered=TRUE)
 
-	# EDW Success
+	# 2017 Profile question EDW: Success
 	df[,9] <- factor(df[,9], levels=c(
 		"Unsure or not applicable",
 		"3 - Advanced stage - we have been using analytics for a long time and are increasingly using advanced and even predictive analytics across a variety of departments",
@@ -89,7 +97,7 @@ cleanProfiles <- function(df){
 		),
 		ordered=TRUE)
 
-	# Analyst involvement
+	# 2017 Profile question Analyst: involvement
 	df[,10] <- factor(df[,10], levels=c(
 		"Unsure or not applicable",
 		"1 - Not involved",
@@ -98,10 +106,10 @@ cleanProfiles <- function(df){
 		),
 		ordered=TRUE)
 
-	# Free text age field needs cleaning
-	df$FavoriteAgeDSC <- ageCleaner(df[,15])
+	# 2017 Profile question Free text age field needs cleaning
+	df$FavoriteAgeDSC <- sanitize_integers_in_free_text(df[,15])
 
-	# Something about being weak
+	# 2017 Profile question Something: about being weak
 	df[,16] <- factor(df[,16], levels=c(
 		"Not applicable",
 		"1 - Weak or below average",
@@ -110,7 +118,7 @@ cleanProfiles <- function(df){
 		),
 		ordered=TRUE)
 
-	# Something about being weak
+	# 2017 Profile question Something: about being weak
 	df[,17] <- factor(df[,17], levels=c(
 		"Unsure or not applicable",
 		"1 - Not successful",
@@ -119,55 +127,53 @@ cleanProfiles <- function(df){
 		),
 		ordered=TRUE)
 
-	# 18 needs to be ordered. Long names
+	# 2017 Profile question 18: needs to be ordered. Long names
 	num <- 18
 	names(df)[num]
 	levels(df[,num])
 	choices <- levels(df[,num])
 	df[,num] <- factor(df[,num], levels = choices[c(3, 4, 1, 2)], ordered = TRUE)
 
-	# 19 is fine
+	# 2017 Profile question 19: is fine
 
-	# 20 needs to be ordered. Long names
+	# 2017 Profile question 20: needs to be ordered. Long names
 	num <- 20
 	names(df)[num]
 	levels(df[,num])
 	choices <- levels(df[,num])
 	df[,num] <- factor(df[,num], levels = choices[c(5, 1, 4, 2, 3)], ordered = TRUE)
 
-	# 21 needs to be ordered. Long names
+	# 2017 Profile question 21: needs to be ordered. Long names
 	num <- 21
 	names(df)[num]
 	levels(df[,num])
 	choices <- levels(df[,num])
 	df[,num] <- factor(df[,num], levels = choices[c(4, 2, 3, 1)], ordered = TRUE)
 
-	# 22 needs to be ordered. Long names
+	# 2017 Profile question 22: needs to be ordered. Long names
 	num <- 22
 	names(df)[num]
 	levels(df[,num])
 	choices <- levels(df[,num])
 	df[,num] <- factor(df[,num], levels = choices[c(3, 1, 2)], ordered = TRUE)
 
-	# 23 needs to be ordered. Long names
+	# 2017 Profile question 23: needs to be ordered. Long names
 	num <- 23
 	names(df)[num]
 	levels(df[,num])
 	choices <- levels(df[,num])
 	df[,num] <- factor(df[,num], levels = choices[c(3, 2, 1, 4)], ordered = TRUE)
 
-	# 24 needs to be ordered. Long names
+	# 2017 Profile question 24: needs to be ordered. Long names
 	num <- 24
 	names(df)[num]
 	levels(df[,num])
 	choices <- levels(df[,num])
 	df[,num] <- factor(df[,num], levels = choices[c(4, 5, 3, 2, 1)], ordered = TRUE)
 
-	# 25 is fine
+	# 2017 Profile question 25: is fine
 
-	df <- dashCleaner(df)
+	df <- remove_dashes_from_strings(df)
 
 	return(df)
 }
-
-
--- pollTest.R
+++ pollTest.R
@@ -1,125 +1,126 @@
 # Script for HAS Polls
+# Authors: Taylor(s), Mike(s)
+#
+# Notes:
+# 	- There are lots of TODOs sprinkled throughout that should be turned into issues and dealt with for 2018
+# 	- This was built with a slightly hacked version of healthcareai 1.0
+# 		+ The hacks were entirely in the findingVariation, so after a refactor, 
+#			this will need to be fixed.
+# 	- The credentials file contains a simple connection string returning function called 'credentials()'
+#		- example: server=<server dns>;database=<database>;uid=<user>;pwd=<password>;Port=1433;driver=FreeTDS;TDS_Version=8.0;
+#			* note the driver here was for macOS. For windows switch to something like {SQL Server 11}
+#	- Note that for speed of hacks the categorical dataframe returns all zeros in the median difference
+#		to make sorting the entire dataframe easier. This should probably be fixed next year.
 
 library(healthcareai)
 library(dplyr)
 library(RODBC)
-setwd('~/repos/healthcareai-r')
-source('common-sql-queries.R')
-source('common-profile-cleaner.R')
-source('./.credentials.R')
+# This needs to be adjusted to your copy of the hacked 1.0 healthcareai TODO remove this hack for 2018
+setwd("~/repos/healthcareai-r")
+source("common-sql-queries.R")
+source("common-profile-cleaner.R")
+source("./.credentials.R")
 
-
-# Profile vs a single poll question id
-singlePollVariance <- function(df1, measureColName, questionID) {
-	glued <- data.frame()
-	res <- list()
-
-	catVarList <- names(Filter(is.factor, df1))
-
-	for (q in catVarList) { 
-		res[[q]] <- variationAcrossGroups(df1, 
-							  categoricalCols = q,
-							  measureCol = measureColName,
-							  sigLevel = .05,
-							  plotGroupDifferences = FALSE,
-							  returnGroupStats = TRUE)
-		res[[q]]$`Multiple-comparison-adjusted p-values for each pair of groups`$profileQuestionNumber <- q
-		glued <- rbind(glued, res[[q]]$`Multiple-comparison-adjusted p-values for each pair of groups`)
-	}
-
-	glued <- glued[order(glued$`Adjusted p-value`),]
-	glued$pollQuestionID <- questionID
-	return(glued)
+# Find variance for an ordinal poll question compared to all profile questions
+single_poll_variance <- function(df1, measure_column_name, question_id) {
+    glued <- data.frame()
+    raw_results <- list()
+    
+    categorical_columns <- names(Filter(is.factor, df1))
+    
+    for (q in categorical_columns) {
+        raw_results[[q]] <- variationAcrossGroups(df1, categoricalCols = q, measureCol = measure_column_name, sigLevel = 0.05, 
+            plotGroupDifferences = FALSE, returnGroupStats = TRUE)
+        # Note the heinously long column name that comes from healthcareai 1.0 TODO change this column after the refactor next year
+        raw_results[[q]]$`Multiple-comparison-adjusted p-values for each pair of groups`$profileQuestionNumber <- q
+        glued <- rbind(glued, raw_results[[q]]$`Multiple-comparison-adjusted p-values for each pair of groups`)
+    }
+    
+    glued <- glued[order(glued$`Adjusted p-value`), ]
+    glued$pollquestion_id <- question_id
+    return(glued)
 }
 
-singlePollCategorical <- function(df1, measureColName, questionID) {
-  
-  require(tidyverse)
-  # ID profile question column numbers (to loop over)
-  profileQs <- which(!names(df1) %in% c("UserID", "UserFullNM", "FavoriteAgeDSC"))
-  
-  responseCol <- grep(measureColName, names(df1)) 
-  profileQs <- profileQs[!profileQs %in% responseCol]
-  out <- 
-    map_df(profileQs, function(Q) {
-      
-      answers <- as.character(unique(df1[[Q]]))
-      answers <- answers[!is.na(answers)]
-      # Get all the pairs of answers, then remove duplicates
-      pairs <- expand.grid(answer1 = answers, answer2 = answers, stringsAsFactors = FALSE)
-      pairs <- pairs[pairs$answer1 < pairs$answer2, ]
-      
-      
-      map_df(seq_len(nrow(pairs)), function(i) {
+# Find variance for a categorical poll question compared to all profile questions
+single_poll_categorical <- function(df1, measure_column_name, question_id) {
+    require(tidyverse)
+    # ID profile question column numbers (to loop over)
+    profile_questions <- which(!names(df1) %in% c("UserID", "UserFullNM", "FavoriteAgeDSC"))
+    
+    responseCol <- grep(measure_column_name, names(df1))
+    profile_questions <- profile_questions[!profile_questions %in% responseCol]
+    out <- map_df(profile_questions, function(Q) {
         
-        fTab <- table(df1[df1[[Q]] %in% pairs[i, ], c(Q, responseCol)])
-        # Remove rows not in the two answers we're interested in
-        fTab <- fTab[rowSums(fTab) > 1, ]
-        if (nrow(fTab) > 2)
-          stop("There should only be two answers here, but there are more.")
+        answers <- as.character(unique(df1[[Q]]))
+        answers <- answers[!is.na(answers)]
+        # Get all the pairs of answers, then remove duplicates
+        pairs <- expand.grid(answer1 = answers, answer2 = answers, stringsAsFactors = FALSE)
+        pairs <- pairs[pairs$answer1 < pairs$answer2, ]
         
-        # Run chi-squared
-        chiSq <- suppressWarnings(chisq.test(fTab))
         
-        data_frame(
-          Groups = paste(pairs[i, ], collapse = "-"),
-          "Mean Difference" = 0,
-          "Adjusted p-value" = chiSq$p.value,
-          profileQuestionNumber = names(df1)[Q],
-          pollQuestionID = questionID
-        ) 
-        
-      })
-    }) 
-  out <- arrange(out, `Adjusted p-value`)
-  return(as.data.frame(out))
+        map_df(seq_len(nrow(pairs)), function(i) {
+            
+            fTab <- table(df1[df1[[Q]] %in% pairs[i, ], c(Q, responseCol)])
+            # Remove rows not in the two answers we're interested in
+            fTab <- fTab[rowSums(fTab) > 1, ]
+            if (nrow(fTab) > 2) 
+                stop("There should only be two answers here, but there are more.")
+            
+            # Run chi-squared
+            chiSq <- suppressWarnings(chisq.test(fTab))
+            
+            data_frame(Groups = paste(pairs[i, ], collapse = "-"), `Mean Difference` = 0, `Adjusted p-value` = chiSq$p.value, 
+                profileQuestionNumber = names(df1)[Q], pollquestion_id = question_id)
+        })
+    })
+    out <- arrange(out, `Adjusted p-value`)
+    return(as.data.frame(out))
 }
 
 
-findVariance <- function(questionID){
-	# setup azure connection from sourced file
-	# questionID = 980
-	tempCredentials = credentials()
-	conn <- odbcDriverConnect(connection = tempCredentials)
-
-	# Get data
-	query <- pollQuestionSQL(questionID)
-	df <- sqlQuery(conn, query)
-	# head(df)
-
-	# Clean the profile data
-	dfClean <- cleanProfiles(df)
-
-	print(paste('number of missing rows:',sum(is.na(dfClean$AnswerNBR)))) # Count of missing answers
-	print(paste('missing rows ratio:', sum(is.na(dfClean$AnswerNBR)) / nrow(dfClean)))  # Proportion of missing answers
-	
-	# Clean the data (and filter out anyone who hasn't answered the questions - NULLS)
-	dfClean <- dfClean[!is.na(dfClean$AnswerNBR), ]
-	print(paste('Found',length(dfClean$UserID), 'answers'))
-
-	# fake up some data
-	# dfClean$AnswerNBR <- sample(1:5, nrow(dfClean), replace=TRUE)
-
-	# png("../../Desktop/tmp.png", width = 400000, height = 40000)
-
-	# Get variance for either ordinal or categorical
-	if (is.numeric(dfClean$AnswerNBR)){
-		variances <- singlePollVariance(dfClean, 'AnswerNBR', questionID)
-		
-	} else {
-		variances <- singlePollCategorical(dfClean, 'AnswerNBR', questionID)
-	}
-
-  # dev.off()
-	return(variances)
+find_variance <- function(question_id) {
+    # setup azure connection from credentials file question_id = 980
+    connection_string = credentials()
+    conn <- odbcDriverConnect(connection = connection_string)
+    
+    # Get data
+    query <- pollQuestionSQL(question_id)
+    df <- sqlQuery(conn, query)
+    
+    # Clean the profile data
+    dfClean <- clean_profiles(df)
+    
+    print(paste("number of missing rows:", sum(is.na(dfClean$AnswerNBR))))  # Count of missing answers
+    print(paste("missing rows ratio:", sum(is.na(dfClean$AnswerNBR))/nrow(dfClean)))  # Proportion of missing answers
+    
+    # Clean the data (and filter out anyone who hasn't answered the questions - NULLS)
+    dfClean <- dfClean[!is.na(dfClean$AnswerNBR), ]
+    print(paste("Found", length(dfClean$UserID), "answers"))
+    
+    # Create some fake data for testing dfClean$AnswerNBR <- sample(1:5, nrow(dfClean), replace=TRUE)
+    
+    # hack for base r box plot in current version findingVariation TODO clean up next year png('../../Desktop/tmp.png', width =
+    # 400000, height = 40000)
+    
+    # Get variance for either ordinal or categorical
+    if (is.numeric(dfClean$AnswerNBR)) {
+        variances <- single_poll_variance(dfClean, "AnswerNBR", question_id)
+    } else {
+        variances <- single_poll_categorical(dfClean, "AnswerNBR", question_id)
+    }
+    
+    # dev.off()
+    return(variances)
 }
 
-# Run this bit here
-q = 978
-results <- findVariance(q)
+# In session running a single poll question
+q = 992
+results <- find_variance(q)
 View(results)
 
+# All poll questions for day 1 bulk analyis
+all_day_one_poll_questions <- c(1156, 912, 977, 978, 979, 1080, 1091, 980, 1083, 1084, 913, 914, 915, 916, 917, 918, 919, 920, 
+    921, 922, 923, 924, 925, 926, 927, 928, 929, 930, 981, 982, 983, 984, 985, 986, 987, 988, 989)
 
-# lapply(dfClean, class)
-# names(dfClean)
-# lapply(dfClean, levels)
+all_results <- map_df(all_day_one_poll_questions, find_variance)
+all_results <- arrange(all_results, `Adjusted p-value`)
